34d1bb1386f4cd0b40f0cf8bd81636dfffcd1da6
Terry Xu <knight3001@gmail.com>
2021-07-29 12:33:39 +1000

    makeRepeated sample list

--- src/mocks/db.ts
+++ src/mocks/db.ts
@@ -4,18 +4,29 @@ import { Post } from "../app/services/posts";
 const db = factory({
   post: {
     id: primaryKey(String),
-    name: String
-  }
+    name: String,
+  },
 });
 
-db.post.create({ id: "1", name: "A sample post" });
-db.post.create({ id: "2", name: "A post about rtk query" });
+const makeRepeated = (arr: Array<string>, repeats: number) =>
+  Array.from({ length: repeats }, () => arr).flat();
+
+makeRepeated(
+  [
+    "A sample post",
+    "A post about RTK Query",
+    "How to randomly throw errors, a novella",
+  ],
+  1
+).forEach((name, index) => {
+  db.post.create({ id: String(index), name: name });
+});
 
 export const handlers = [
   rest.put("/posts/:id", (req, res, ctx) => {
     const { name } = req.body as Partial<Post>;
 
-    if (Math.random() < 1) {
+    if (Math.random() < 0.5) {
       return res(
         ctx.json({ error: "Oh no, there was an error" }),
         ctx.status(500),
@@ -24,10 +35,10 @@ export const handlers = [
     }
     const post = db.post.update({
       where: { id: { equals: req.params.id } },
-      data: { name }
+      data: { name },
     });
 
     return res(ctx.json(post), ctx.delay(400));
   }),
-  ...db.post.toHandlers("rest")
+  ...db.post.toHandlers("rest"),
 ] as const;
