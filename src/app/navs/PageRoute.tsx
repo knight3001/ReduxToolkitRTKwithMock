ed321411fe9ab1b10b1815ddae65b47069944ac9
Terry Xu <knight3001@gmail.com>
2021-11-30 15:08:07 +1000

    play with mui v5 Autocomplete

--- src/app/navs/PageRoute.tsx
+++ src/app/navs/PageRoute.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { PostsManager } from "../../features/posts/PostsManager";
 import Template from "./Template";
+import MultiAutoComplete from "../components/MultiAutoComplete";
 
 const PageRoute = () => (
   <BrowserRouter>
     <Template>
       <Switch>
+        <Route path="/mui" component={MultiAutoComplete} />
         <Route path="/" component={PostsManager} />
       </Switch>
     </Template>
